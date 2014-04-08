9bca7b60ea61dc9a5c581883004debd6fc2730d5
Chris Adams <chris@improbable.org>
2014-04-08 13:34:27 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='image-mining',
+      version='0.1.0',
+      author='Chris Adams',
+      author_email='chris@improbable.org',
+      packages=['image_mining'],
+      scripts=['bin/extract-figures.py', 'bin/locate-thumbnail.py'],
+      url='https://github.com/acdha/image-mining/',
+      license='LICENSE.txt',
+      description='Extract useful information from scanned images using OpenCV',
+      long_description=open('README.rst').read(),
+      install_requires=['numpy'])
