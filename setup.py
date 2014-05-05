36b8eb605b6f51d64773fd9ee27fe9c60efccf46
Chris Adams <chris@improbable.org>
2014-05-05 14:15:41 -0400

    v0.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='image-mining',
-      version='0.1.3',
+      version='0.1.4',
       author='Chris Adams',
       author_email='chris@improbable.org',
       packages=['image_mining'],
